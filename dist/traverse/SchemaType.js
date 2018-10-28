22b311e7fb09b44131cf3c085b08487556ac734a
Jiri Smolik <jiri.smolik@ackee.cz>
2018-10-28 14:15:06 +0100

    Reinstall update dist ignore

--- dist/traverse/SchemaType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SchemaType;
-(function (SchemaType) {
-    SchemaType[SchemaType["primitive"] = 0] = "primitive";
-    SchemaType[SchemaType["object"] = 1] = "object";
-    SchemaType[SchemaType["array"] = 2] = "array";
-})(SchemaType || (SchemaType = {}));
-exports.default = SchemaType;
